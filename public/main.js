5098464505d6af2be8a08f4a9b58b74e04153fee
adrian <nyancatgaming9999@gmail.com>
2023-02-02 14:59:16 -0800

    footer

--- public/main.js
+++ public/main.js
@@ -2,10 +2,9 @@ const cover = document.getElementById("cover");
 let cover1 = document.getElementById("cover1"),
     cover2 = document.getElementById("cover2");
 
-const backgroundImages = new Array(22).fill().map((_, i) => `images/wide${i}.jpg`);
 const getBackground = () => {
-    const image = backgroundImages.shift();
-    backgroundImages.push(image);
+    const image = DATA.backgrounds.shift();
+    DATA.backgrounds.push(image);
     return image;
 };
 
@@ -28,3 +27,48 @@ const updateCoverImage = () => {
 cover1.style.backgroundImage = `url(${getBackground()})`;
 cover2.style.backgroundImage = `url(${getBackground()})`;
 setInterval(updateCoverImage, 5000);
+
+const columnContainer = document.getElementById("columns-container");
+const columns = [];
+
+for(let i = 0; i < 10; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    columnContainer.append(column);
+    columns.push(column);
+}
+
+const buildMasonry = () => {
+
+    const numColumns = Math.floor(window.innerWidth / 300);
+
+    for(let i = 0; i < columns.length; i++) {
+        columns[i].style.display = i < numColumns ? "" : "none";
+        columns[i].replaceChildren();
+    }
+
+    const heights = new Array(numColumns).fill(0)
+    for(const image of DATA.images) {
+
+        // find the shortest column
+        let shortest = 0, shortestHeight = Infinity;
+        for(let i = 0; i < heights.length; i++) {
+            if(heights[i] < shortestHeight) {
+                shortest = i;
+                shortestHeight = heights[i];
+            }
+        }
+
+        // add image
+        const img = document.createElement("img");
+        img.src = image.url;
+        img.loading = "lazy";
+        columns[shortest].append(img);
+        heights[shortest] += image.height;
+
+    }
+
+};
+
+buildMasonry();
+window.addEventListener("resize", buildMasonry, {passive: true});
--- rescale-images.js
+++ rescale-images.js
@@ -2,12 +2,29 @@ const fs = require("fs"),
       sharp = require("sharp");
 
 (async () => {
+
     let index = 0,
-        wideindex = 0;
+        backgroundIndex = 0;
+
+    const data = {
+        images: [],
+        backgrounds: []
+    };
+
     for(const image of fs.readdirSync("originals")) {
-        const properties = await sharp("originals/" + image).resize({width: 640}).toFile(`public/images/image${index++}.jpg`);
+        
+        const url = `images/image${index++}.jpg`;
+        const properties = await sharp("originals/" + image).resize({width: 640}).toFile("public/" + url);
+        data.images.push({url, width: properties.width, height: properties.height});
+
         if(properties.width > properties.height) {
-            await sharp("originals/" + image).resize({width: 1920}).toFile(`public/images/wide${wideindex++}.jpg`)
+            const backgroundUrl = `images/background${backgroundIndex++}.jpg`;
+            data.backgrounds.push(backgroundUrl);
+            await sharp("originals/" + image).resize({width: 1920}).toFile("public/" + backgroundUrl);
         }
+
     }
+
+    fs.writeFileSync("public/images/data.js", "const DATA = " + JSON.stringify(data));
+
 })();
